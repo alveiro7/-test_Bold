2c72968e4651e1ebfa69e62e1acb08b945c0bbb9
Alveiro H <alveiro7@gmail.com>
2021-10-10 21:55:58 -0500

    :bug: fetch fake api completed

--- src/components/FilterSalesModal/FilterSalesModal.jsx
+++ src/components/FilterSalesModal/FilterSalesModal.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import './FilterSalesModal.scss'
 
-const FilterSalesModal = () => (
-        <button>
+const FilterSalesModal = ({showFilter, setShowFilter}) => {
+
+    return (
+        <>
+
+        <button onClick={() => setShowFilter(!showFilter)}  >
             Filtrar
         </button>
+        <section id='portal'>
+        <div className="tooltiptext">
+    <div className="cont_check">
+        <input type="checkbox" className="checkbox" id="check1" name="check1" />
+        <label htmlFor="check1">Cobro con datafono</label>
+    </div>
+    <div className="cont_check">
+        <input type="checkbox" className="checkbox" id="check2" name="check2" />
+        <label htmlFor="check2">Cobro con link de pago</label>
+    </div>
+    <div className="cont_check">
+        <input type="checkbox" className="checkbox" id="check3" name="check3" />
+        <label htmlFor="check3">Ver todos</label>
+    </div>
+    <button className="btn3">Aplicar</button>
+
+</div>
+        </section>
+        </>
 )
+}
 
 export default FilterSalesModal
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useState, useEffect } from 'react'
+
 import Header from '../components/Header/Header'
 import TotalSales from '../components/TotalSales/TotalSales'
 import TimeSales from '../components/TimeSales/TimeSales'
@@ -9,14 +10,16 @@ import YourSales from '../components/YourSales/YourSales'
 import './App.scss'
 
 
+
 const App = () => {
 
     const [time, setTime] = useState(option)
     const option = [{id: 1, name:"hoy"}, {id:2, name:"esta semana"}, {id:3, name: "septiembre"}]
 
+    const [showFilter, setShowFilter] = useState(false)
+
     const [payment, setPayment]=useState([]);
     const [paymentsToShow, setPaymentstoShow] = useState([]);
-
 
     const API_URL = 'http://localhost:3004/payments';
 
@@ -49,7 +52,7 @@ const App = () => {
             <Header />
             <TotalSales time={time} />
             <TimeSales time={time} setTime={setTime}  />
-            <FilterSalesModal />
+            <FilterSalesModal showFilter={showFilter} setShowFilter={setShowFilter} />
             <YourSales time={time} payment={payment} />
         </Fragment>
     )
